f640d09a82868fe06475317ed72ace54c09aef8d
sourabhmv <65846282+sourabhmv@users.noreply.github.com>
2021-09-21 15:08:13 +0430

    Update Emailsnd.py

--- Emailsnd.py
+++ Emailsnd.py
@@ -5,11 +5,11 @@ import smtplib
 server=smtplib.SMTP('smtp.gmail.com' , 587)
 server.starttls()
 
-server.login("sender_mail", "password")
-subject = 'This is jenkins task 2'
+server.login("sourabhmvmo@gmail.com", "iamiRonman#24@")
+subject = 'web site error'
 
 msg = "There is somthing wrong in your code"
 
-server.sendmail("sender_mail", "receiver_mail", msg)
+server.sendmail("sourabhmvmo@gmail.com", "sourabhclasher@gmail.com", msg)
 print("Email sent successfully!")
 server.quit()
