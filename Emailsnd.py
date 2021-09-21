96fff9006365384f5c7782204b0086348714ce04
sourabhmv <65846282+sourabhmv@users.noreply.github.com>
2021-09-21 15:27:22 +0430

    Update Emailsnd.py

--- Emailsnd.py
+++ Emailsnd.py
@@ -1,15 +1,15 @@
-#!bin/bash/python5
+#!bin/bash/python3
 
 import smtplib
 
 server=smtplib.SMTP('smtp.gmail.com' , 587)
 server.starttls()
 
-server.login("sourabhmvmo@gmail.com", "iamiRonman#24@")
-subject = 'web site error'
+server.login("sender_mail", "password")
+subject = 'Error in your web site'
 
-msg = "There is somthing wrong in your code"
+msg = "Web site is not working"
 
-server.sendmail("sourabhmvmo@gmail.com", "sourabhclasher@gmail.com", msg)
+server.sendmail("sender_mail", "receiver_mail", msg)
 print("Email sent successfully!")
 server.quit()
