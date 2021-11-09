af18f0f4fcc29a7401533723b2a8f9ac64ba1791
sourabhmv <65846282+sourabhmv@users.noreply.github.com>
2021-11-09 14:08:21 +0530

    Update Emailsnd.py

--- Emailsnd.py
+++ Emailsnd.py
@@ -5,11 +5,11 @@ import smtplib
 server=smtplib.SMTP('smtp.gmail.com' , 587)
 server.starttls()
 
-server.login("sender_mail", "password")
+server.login("sourabhclasher@gmail.com", "CLASHEr#24@")
 subject = "Error in your web site"
 
 msg = "Web site is not working"
 
-server.sendmail("sender_mail", "receiver_mail", msg)
+server.sendmail("sourabhclasher@gmail.com", "sourabhmvmo@gmail.com", msg)
 print("Email sent successfully!")
 server.quit()
